4df63a6b7c8e1fdab2c8827c93ac8908b9282704
Michael Feveile Mariboe <mmari@dfds.com>
2021-08-30 16:08:25 +0200

    Outputted pswds from tf Added K8s secret for pswds Updated Helm values YAML Added Azure Pipeline YAML

---
+++ terraform/output.tf
@@ -0,0 +1,26 @@
+output "es_host" {
+  value = aws_elasticsearch_domain.datahub-es.endpoint
+}
+
+output "es_password" {
+  value = module.es-password.password
+  sensitive = true
+}
+
+output "db_host" {
+  value = module.db.db_instance_endpoint
+}
+
+output "db_port" {
+  value = module.db.db_instance_port
+}
+
+output "db_username" {
+  value = module.db.db_instance_username
+  sensitive = true
+}
+
+output "db_password" {
+  value = module.db.db_instance_password
+  sensitive = true
+}
